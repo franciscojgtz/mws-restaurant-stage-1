54117f912c9feebfb4c18a50a5c53cb7142b280b
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2018-05-21 16:57:28 -0700

    Fix: Change text in restaurant list from view details to restaurant details

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'Restaurant details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
